e1aab447232bb8133ca4753da47160f64017f3c9
Jonathan Felix <jhow.wi56@gmail.com>
2021-08-22 21:01:31 -0300

    Created project

---
+++ src/test/java/com/esturando/PrimeiroProjeto/PrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esturando.PrimeiroProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
