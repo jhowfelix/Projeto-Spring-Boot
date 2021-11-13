0b1f73649d8b02e8cd7c77db289e762003a939a9
Jonathan Felix <jhow.wi56@gmail.com>
2021-11-13 16:35:00 -0300

    exception handling byId

--- src/main/java/com/esturando/PrimeiroProjeto/services/UserServices.java
+++ src/main/java/com/esturando/PrimeiroProjeto/services/UserServices.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.esturando.PrimeiroProjeto.entities.User;
 import com.esturando.PrimeiroProjeto.repositories.UserRepository;
+import com.esturando.PrimeiroProjeto.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserServices {
@@ -21,7 +22,7 @@ public class UserServices {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
---
+++ src/main/java/com/esturando/PrimeiroProjeto/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.esturando.PrimeiroProjeto.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("ResourceNotFound. Id " + id);
+	}
+	
+
+}
