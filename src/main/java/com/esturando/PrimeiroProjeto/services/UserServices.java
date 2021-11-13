4562cd0083179c1cb95b85d0f76dd50bbc940196
Jonathan Felix <jhow.wi56@gmail.com>
2021-11-13 16:48:44 -0300

    exception handling delete

--- src/main/java/com/esturando/PrimeiroProjeto/services/UserServices.java
+++ src/main/java/com/esturando/PrimeiroProjeto/services/UserServices.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.esturando.PrimeiroProjeto.entities.User;
 import com.esturando.PrimeiroProjeto.repositories.UserRepository;
+import com.esturando.PrimeiroProjeto.services.exceptions.DatabaseException;
 import com.esturando.PrimeiroProjeto.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -24,27 +27,33 @@ public class UserServices {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User user) {
 		return repository.save(user);
 	}
-	
+
 	public User update(Long id, User user) {
 		User usuario = repository.getOne(id);
 		updateDate(usuario, user);
 		return repository.save(usuario);
-		
+
 	}
-	
+
 	private void updateDate(User usuario, User user) {
 		usuario.setName(user.getName());
 		usuario.setEmail(user.getEmail());
 		usuario.setPhone(user.getPhone());
-		
+
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 }
